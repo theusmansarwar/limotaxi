5664c0bb6b8f19c100c685f3855d6db58d512c0f
RahmaSohail <rahmasohail70@gmail.com>
2025-11-11 11:31:00 +0500

    k

--- src/pages/dispatchsDashboard/DispatchDashboard.jsx
+++ src/pages/dispatchsDashboard/DispatchDashboard.jsx
@@ -1,20 +1,18 @@
 import React from 'react'
 import BookingForm from "../../components/BookingForm/BookingForm";
 import Dispatch from "../../components/dispatchDashboard/Dispatch";
-
+import './DispatchDashboard.css';
 
 const DispatchDashboard = () => {
   return (
-
-
-    <div className="main-wrapper">
-   
-         <BookingForm />
+    <div className="dispatch-page-wrapper">
+      <div className="booking-column">
+        <BookingForm />
+      </div>
+      <div className="dispatch-column">
         <Dispatch />
-    
-</div>
-
-   
+      </div>
+    </div>
   )
 }
 
